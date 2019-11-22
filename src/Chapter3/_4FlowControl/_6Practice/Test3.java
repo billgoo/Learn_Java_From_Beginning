27d41b736eed4b4d9c21ee814a5cc0909e64ecd4
billgoo <billgoo0813@gmail.com>
2019-11-22 18:32:34 -0500

    2019.11.22

---
+++ src/Chapter3/_4FlowControl/_6Practice/Test3.java
@@ -0,0 +1,19 @@
+package Chapter3._4FlowControl._6Practice;
+/**
+ * 打印 1+1/2！+1/3！+...+1/20！
+ */
+
+public class Test3 {
+    public static void main(String[] args) {
+        double result = 0, i = 1;
+        while (i <= 20) {
+            double down = 1;
+            for (int j = 1; j <= i; j++) {
+                down *= j;
+            }
+            result += 1 / down;
+            i++;
+        }
+        System.out.println(result);
+    }
+}
