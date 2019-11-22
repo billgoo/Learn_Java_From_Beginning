edb72c42f25f9f21d965dc8bc9475418bca0f140
billgoo <billgoo0813@gmail.com>
2019-11-22 18:24:54 -0500

    2019.11.22

---
+++ src/Chapter3/_4FlowControl/_6Practice/Test2.java
@@ -0,0 +1,21 @@
+package Chapter3._4FlowControl._6Practice;
+/**
+ * 打印菱形
+ */
+
+public class Test2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= Math.abs(2 - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 5 - 2 * Math.abs(2 - i); j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j <= Math.abs(2 - i); j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
