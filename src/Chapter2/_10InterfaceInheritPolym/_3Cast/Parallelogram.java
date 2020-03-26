aee1453d154abcf299209a29ab399b45f1cc3de3
billgoo <billgoo0813@gmail.com>
2020-03-26 12:16:38 +0800

    2020.3.26

---
+++ src/Chapter2/_10InterfaceInheritPolym/_3Cast/Parallelogram.java
@@ -0,0 +1,19 @@
+package Chapter2._10InterfaceInheritPolym._3Cast;
+class Quadrangle {                                  // 四边形类
+    public static void draw(Quadrangle q){          // 四边形类中的方法
+        System.out.println(q.getClass().getName());
+    }
+}
+
+public class Parallelogram extends Quadrangle {     // 平行四边形类
+    public static void main(String[] args) {
+        Parallelogram p = new Parallelogram();      // 实例化平行四边形类的对象
+        draw(p);                                    // 调用父类方法
+        /**
+         * output:
+         * Chapter2._10InterfaceInheritPolym._3Cast.Parallelogram
+         *
+         * 对象向上转型的例子
+         */
+    }
+}
